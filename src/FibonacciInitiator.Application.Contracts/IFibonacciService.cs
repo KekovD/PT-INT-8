6e4497b2197a44179acce016d55a7353dfc2aa5b
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-06-10 23:16:28 +0300

    refactor: remove FibonacciInitiator.Application

--- src/FibonacciInitiator.Application.Contracts/IFibonacciService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using FibonacciInitiatorService.Application.Models;
-
-namespace FibonacciInitiatorService.Application.Contracts;
-
-public interface IFibonacciService
-{
-    public Task<FibonacciResult> CalculateNext(FibonacciResult previousResult);
-}
--- src/FibonacciInitiator.Application.Models/FibonacciResult.cs
+++
@@ -1,3 +0,0 @@
-namespace FibonacciInitiatorService.Application.Models;
-
-public record FibonacciResult(int Previous, int Current);
