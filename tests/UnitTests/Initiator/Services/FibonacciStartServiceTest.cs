b6c93b22de2de109345a36e8adfdd082c089a4f5
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-06-13 20:10:27 +0300

    test: fix code style

--- tests/UnitTests/Initiator/Services/FibonacciStartServiceTest.cs
+++ tests/UnitTests/Initiator/Services/FibonacciStartServiceTest.cs
@@ -4,57 +4,56 @@ using SharedModels;
 using System;
 using System.Threading.Tasks;
 
-namespace Tests.Initiator.Services
+namespace Tests.Initiator.Services;
+
+public class FibonacciStartServiceTest
 {
-    public class FibonacciStartServiceTest
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    public async Task StartCalculationsAsync_ValidNumberOfLaunches_SendsRequests(int numberOfLaunches)
     {
-        [Theory]
-        [InlineData(5)]
-        [InlineData(10)]
-        public async Task StartCalculationsAsync_ValidNumberOfLaunches_SendsRequests(int numberOfLaunches)
-        {
-            var mockHttpClientService = new Mock<IHttpClientService>();
-            var mockMessageQueueService = new Mock<IMessageQueueService>();
-            var mockLogStrategy = new Mock<ILogStrategy>();
+        var mockHttpClientService = new Mock<IHttpClientService>();
+        var mockMessageQueueService = new Mock<IMessageQueueService>();
+        var mockLogStrategy = new Mock<ILogStrategy>();
 
-            int startPrevious = 0;
-            int startCurrent = 1;
+        int startPrevious = 0;
+        int startCurrent = 1;
 
-            var fibonacciService = new FibonacciStartService(
-                mockHttpClientService.Object,
-                mockMessageQueueService.Object,
-                startPrevious,
-                startCurrent,
-                mockLogStrategy.Object);
+        var fibonacciService = new FibonacciStartService(
+            mockHttpClientService.Object,
+            mockMessageQueueService.Object,
+            startPrevious,
+            startCurrent,
+            mockLogStrategy.Object);
 
-            await fibonacciService.StartCalculationsAsync(numberOfLaunches);
+        await fibonacciService.StartCalculationsAsync(numberOfLaunches);
 
-            mockHttpClientService.Verify(
-                svc => svc.SendStateToCalculatorAsync(It.IsAny<FibonacciState>()),
-                Times.Exactly(numberOfLaunches));
-        }
-        
-        [Theory]
-        [InlineData(0)]
-        [InlineData(-1)]
-        public async Task StartCalculationsAsync_InvalidNumberOfLaunches_ThrowsException(int numberOfLaunches)
-        {
-            var mockHttpClientService = new Mock<IHttpClientService>();
-            var mockMessageQueueService = new Mock<IMessageQueueService>();
-            var mockLogStrategy = new Mock<ILogStrategy>();
+        mockHttpClientService.Verify(
+            svc => svc.SendStateToCalculatorAsync(It.IsAny<FibonacciState>()),
+            Times.Exactly(numberOfLaunches));
+    }
 
-            int startPrevious = 0;
-            int startCurrent = 1;
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public async Task StartCalculationsAsync_InvalidNumberOfLaunches_ThrowsException(int numberOfLaunches)
+    {
+        var mockHttpClientService = new Mock<IHttpClientService>();
+        var mockMessageQueueService = new Mock<IMessageQueueService>();
+        var mockLogStrategy = new Mock<ILogStrategy>();
 
-            var fibonacciService = new FibonacciStartService(
-                mockHttpClientService.Object,
-                mockMessageQueueService.Object,
-                startPrevious,
-                startCurrent,
-                mockLogStrategy.Object);
+        int startPrevious = 0;
+        int startCurrent = 1;
 
-            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
-                async () => await fibonacciService.StartCalculationsAsync(numberOfLaunches));
-        }
+        var fibonacciService = new FibonacciStartService(
+            mockHttpClientService.Object,
+            mockMessageQueueService.Object,
+            startPrevious,
+            startCurrent,
+            mockLogStrategy.Object);
+
+        await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+            async () => await fibonacciService.StartCalculationsAsync(numberOfLaunches));
     }
 }
