1ccb0095bfae64f0d05663e6670ecbba60d86662
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-06-14 16:50:20 +0300

    test: add InitiatorControllerTest

---
+++ tests/UnitTests/Initiator/Controllers/InitiatorControllerTest.cs
@@ -0,0 +1,51 @@
+using System;
+using Initiator.Controllers;
+using Initiator.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Tests.Initiator.Controllers;
+
+public class InitiatorControllerTest
+{
+    [Fact]
+    public async Task StartCalculationsAsync_ReturnsOkResult()
+    {
+        var mockFibonacciStartService = new Mock<IFibonacciStartService>();
+        mockFibonacciStartService
+            .Setup(service => service.StartCalculationsAsync(It.IsAny<int>()))
+            .Returns(Task.CompletedTask);
+
+        var controller = new InitiatorController(mockFibonacciStartService.Object);
+
+        int numberOfLaunches = 5;
+
+        var result = await controller.StartCalculationsAsync(numberOfLaunches);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal("Calculations started", okResult.Value);
+
+        mockFibonacciStartService.Verify(service => service.StartCalculationsAsync(numberOfLaunches), Times.Once);
+    }
+    
+    
+    [Fact]
+    public async Task StartCalculationsAsync_ReturnsBadRequestOnException()
+    {
+        var mockFibonacciStartService = new Mock<IFibonacciStartService>();
+        mockFibonacciStartService
+            .Setup(service => service.StartCalculationsAsync(It.IsAny<int>()))
+            .ThrowsAsync(new Exception("Test exception"));
+
+        var controller = new InitiatorController(mockFibonacciStartService.Object);
+
+        int numberOfLaunches = 5;
+
+        var result = await controller.StartCalculationsAsync(numberOfLaunches);
+
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal("An error occurred while starting calculations", badRequestResult.Value);
+
+        mockFibonacciStartService.Verify(service => service.StartCalculationsAsync(numberOfLaunches), Times.Once);
+    }
+}
