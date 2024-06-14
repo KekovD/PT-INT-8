1998f6acb4fdb6a9367be9dfa24048bec6c012b6
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-06-14 17:02:26 +0300

    test: add CalculateNextAsync_ThrowsFormatException_WhenParsingFails to CalculateNextServiceTest

--- tests/UnitTests/Initiator/Services/CalculateNextServiceTest.cs
+++ tests/UnitTests/Initiator/Services/CalculateNextServiceTest.cs
@@ -10,11 +10,12 @@ public class CalculateNextServiceTest
     [Fact]
     public async Task CalculateNextAsync_ReturnsStateUnchanged_WhenAlreadyMatchingStart()
     {
+        var mockLogger = new Mock<ILogStrategy>();
         var startPrevious = "0";
         var startCurrent = "1";
         int startId = 0;
         var state = new FibonacciState(Previous: "0", Current: "1", startId, DateTime.Now);
-        var service = new CalculateNextService(startPrevious, startCurrent);
+        var service = new CalculateNextService(startPrevious, startCurrent, mockLogger.Object);
 
         var result = await service.CalculateNextAsync(state);
 
@@ -24,11 +25,12 @@ public class CalculateNextServiceTest
     [Fact]
     public async Task CalculateNextAsync_ReturnsNewState_WhenNotMatchingStart()
     {
+        var mockLogger = new Mock<ILogStrategy>();
         var startPrevious = "0";
         var startCurrent = "1";
         int startId = 0;
         var state = new FibonacciState(Previous: "1", Current: "1", startId, DateTime.Now);
-        var service = new CalculateNextService(startPrevious, startCurrent);
+        var service = new CalculateNextService(startPrevious, startCurrent, mockLogger.Object);
 
         var result = await service.CalculateNextAsync(state);
 
@@ -36,5 +38,21 @@ public class CalculateNextServiceTest
         Assert.Equal("1", result.Previous);
         Assert.Equal("2", result.Current);
         Assert.Equal(state.StartId, result.StartId);
+    }
+    
+    [Fact]
+    public async Task CalculateNextAsync_ThrowsFormatException_WhenParsingFails()
+    {
+        var mockLogger = new Mock<ILogStrategy>();
+        var startPrevious = "0";
+        var startCurrent = "1";
+        int startId = 0;
+        var invalidState = new FibonacciState(Previous: "not_a_number", Current: "1", startId, DateTime.Now);
+        var service = new CalculateNextService(startPrevious, startCurrent, mockLogger.Object);
+
+        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await service.CalculateNextAsync(invalidState);
+        });
     }
 }
